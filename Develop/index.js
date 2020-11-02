b25c2f2f8f5b51a8ebd2a2828a79ff38772e7324
Kelsea Fields <kelsea.fields@yahoo.com>
2020-11-02 12:45:29 -0800

    comments

--- Develop/index.js
+++ Develop/index.js
@@ -1,63 +1,79 @@
-var inquirer = require("inquirer");
-var fs = require("fs");
+// creating variable and using require function to bring in Inquirer
+const inquirer = require("inquirer");
+// creating variable and using require function to bring in fs
+const fs = require("fs");
+// creating variable and using require to use npm's path//doesn't need install//part of npm
 const path = require("path");
+// creating variable and requiring generateMarkdown.js 
 var generateMarkdown = require("./utils/generateMarkdown");
 
+// remove me
 // array of questions for user
-    const questions = [
-    ];
+    // const questions = [
+    // ];
     
-// function to write README file
+// function to write README file takes in fileName and user data parameters
 function writeToFile(fileName, data) {
   // var newFile = `${data.title.toUpperCase().split(' ').join('')}.md`; 
   
+  // writing file and syncing it using path to join the current working directory using the fileName and user data.
   fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
+  // inquirer prompted questions for user//VIA command-line prompts
   inquirer.prompt([
     {
+      // Takes user text input
       type: "input",
       name: "username",
       message: "Enter Your Github Username"
     },
     {
+      // Takes user text input
       type: "input",
       name: "email",
       message: "Enter Your Email Address"
     },
     {
+      // Takes user text input
       type: "input",
       name: "title",
       message: "What's Your Projects Name?"
       },
       {
+        // Takes user text input
         type: "input",
         name: "description",
         message: "Enter a descrition"
       },
       {
+        // Takes user text input
         type: "input",
         name: "installation",
         message: "Enter Installation Instructions"
       },
       {
+        // Takes user text input
         type: "input",
         name: "usage",
         message: "Enter Usage Info"
       },
       {
+        // Takes user text input
           type: "input",
           name: "contribution",
           message: "Enter Contribution Guidelines"
       },
       {
+        // Takes user text input
           type: "input",
           name: "tests",
           message: "Test Instructions"
       },
-      {
+      { 
+        // Takes user input via checkbox using spacebar to add choice and up and down keys to move down list
           type: "checkbox",
           message: "Licensing Options",
           name: "license",
@@ -76,9 +92,11 @@ function init() {
               "the Unilicense"
             ]
         }
-        
+        // Where the user input is stored (data)
     ]).then(function(data) {
+      // calling function writeToFile(fileName, data) using "README.md" and generateMarkdown(data) parameters & uses a spread opperater to spread data. 
       writeToFile("README.md", generateMarkdown({...data}));
+      // remove me
       console.log(data.license);
   
 });
