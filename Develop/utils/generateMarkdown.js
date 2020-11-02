49c2292149873f05edc5e60f70d8c34d58e7ae25
Kelsea Fields <kelsea.fields@yahoo.com>
2020-11-01 18:17:10 -0800

    writing template for markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-   
-## usage
+----
 
-# ${data.email}
+[${data.license}](badge content)
+
+## Table of Contents
+- Description
+- Installation
+- Usage
+- Contribution
+- Test Instructions
+- contact Information
+
+### Description
+${data.description}
+
+### Installation
+${data.installation}
+
+### Usage
+${data.usage}
+
+### Contribution
+${data.contribution}
+
+### Test Instructions
+${data.tests}
+
+### Contact Information
+[Github Profile](https://github.com/${data.username})
+${data.email}
+
 `;
 }
 
